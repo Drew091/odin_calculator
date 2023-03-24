e9578913f28a4301f6a8412f05147136cfbca118
Drew091 <hoppal.andras91@gmail.com>
2023-03-24 14:43:05 +0100

    Create basic math calculation functions and Create calculator apperience

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # odin_calculator
+
+In this project I will build a calculator which will do all basic calculation.
+
+This calculator will be displayed on a webpage and will look like as real life
+calculator.
---
+++ calculator.js
@@ -0,0 +1,36 @@
+let firstNumber = 0;
+let operator = "";
+let secondNumber = 0;
+
+function add(a,b) {
+  return a+b;
+}
+
+function substract(a,b) {
+  return a-b;
+}
+
+function multiply (a,b) {
+  return a*b;
+}
+
+function divide (a,b) {
+  return a/b;
+}
+
+function operate(operator,firstNumber,secondNumber) {
+  switch (operator) {
+    case "+":
+      return add(firstNumber,secondNumber);
+      break;
+    case "-":
+      return substract(firstNumber,secondNumber);
+      break;
+    case "*":
+      return multiply(firstNumber,secondNumber);
+      break;
+    case "*":
+      return divide(firstNumber,secondNumber);
+  }
+
+}
