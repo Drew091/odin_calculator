f20d6ce8536bd2823d451c24545c45ce1e7dcbef
Drew091 <hoppal.andras91@gmail.com>
2023-03-25 10:11:16 +0100

    Create and Add to buttons sendToDisplay() which do basic calc on display

--- calculator.js
+++ calculator.js
@@ -1,6 +1,11 @@
 let firstNumber = 0;
 let operator = "";
 let secondNumber = 0;
+let displayValue = document.getElementById("displayValue").innerHTML ;
+let lastInput;
+let currentInput;
+let currentOperator = "";
+let lastOperator = "";
 
 function add(a,b) {
   return a+b;
@@ -29,16 +34,123 @@ function operate(operator,firstNumber,secondNumber) {
     case "*":
       return multiply(firstNumber,secondNumber);
       break;
-    case "*":
+    case "/":
       return divide(firstNumber,secondNumber);
   }
 }
 
 function sendToDisplay(value){
-  displayValue = document.getElementById("displayValue").innerHTML;
-  if (displayValue == 0){
+  if (firstNumber == 0 && lastOperator == "" && (value == "+" || value == "-" || value == "/" || value == "*")){
+    firstNumber = parseFloat(displayValue);
+    currentOperator = value;
+    displayValue = 0;
+    console.log("----------");
+    console.log("1st");
+    console.log("firstNumber:");
+    console.log(firstNumber);
+    console.log("secondNumber:");
+    console.log(secondNumber);
+    console.log("displayValue:");
+    console.log(displayValue);
+    console.log("currentOperator:");
+    console.log(currentOperator);
+    console.log("lastOperator:");
+    console.log(lastOperator);
+ }else if (secondNumber == 0 && lastOperator != "" && (value == "+" || value == "-" || value == "/" || value == "*")){
+   displayValue = document.getElementById("displayValue").innerHTML;
+   secondNumber = parseFloat(displayValue);
+   firstNumber = parseFloat(operate(lastOperator,firstNumber,secondNumber));
+   document.getElementById("displayValue").innerHTML = firstNumber ;
+   secondNumber = 0;
+   displayValue = 0;
+   lastOperator = "";
+   currentOperator = value;
+   console.log("----------");
+   console.log("1.5nd")
+   console.log("firstNumber:");
+   console.log(firstNumber);
+   console.log("secondNumber:");
+   console.log(secondNumber);
+   console.log("displayValue:");
+   console.log(displayValue);
+   console.log("currentOperator:");
+   console.log(currentOperator);
+   console.log("lastOperator:");
+   console.log(lastOperator);
+ }else if ((value == "+" || value == "-" || value == "/" || value == "*")){
+    currentOperator = value;
+    displayValue = 0;
+    console.log("----------");
+    console.log("2nd");
+    console.log("firstNumber:");
+    console.log(firstNumber);
+    console.log("secondNumber:");
+    console.log(secondNumber);
+    console.log("displayValue:");
+    console.log(displayValue);
+    console.log("currentOperator:");
+    console.log(currentOperator);
+    console.log("lastOperator:");
+    console.log(lastOperator);
+  }else if (value == "=" && lastOperator == ""){
+    document.getElementById("displayValue").innerHTML = 0;
+
+  }else if (secondNumber == 0 && lastOperator != "" && value == "="){
+    displayValue = document.getElementById("displayValue").innerHTML;
+    secondNumber = parseFloat(displayValue);
+    firstNumber = parseFloat(operate(lastOperator,firstNumber,secondNumber));
+    document.getElementById("displayValue").innerHTML = firstNumber ;
+    displayValue = 0;
+    console.log("----------");
+    console.log("3rd");
+    console.log("firstNumber:");
+    console.log(firstNumber);
+    console.log("secondNumber:");
+    console.log(secondNumber);
+    console.log("displayValue:");
+    console.log(displayValue);
+    console.log("lastOperator:");
+    console.log(lastOperator);
+
+  }else if (value == "="){
+    displayValue = document.getElementById("displayValue").innerHTML;
+    firstNumber = parseFloat(operate(lastOperator,firstNumber,secondNumber));
+    document.getElementById("displayValue").innerHTML = firstNumber ;
+    displayValue = 0;
+    console.log("----------");
+    console.log("4th");
+    console.log("firstNumber:");
+    console.log(firstNumber);
+    console.log("secondNumber:");
+    console.log(secondNumber);
+    console.log("displayValue:");
+    console.log(displayValue);
+    console.log("lastOperator:");
+    console.log(lastOperator);
+
+  }else if (displayValue == 0){
     document.getElementById("displayValue").innerHTML = value;
-  }else{
-    document.getElementById("displayValue").innerHTML += value;
+    displayValue = document.getElementById("displayValue").innerHTML;
+    lastOperator = currentOperator;
+    console.log("----------");
+    console.log("5th");
+    console.log("firstNumber:");
+    console.log(firstNumber);
+    console.log("secondNumber:");
+    console.log(secondNumber);
+    console.log("displayValue:");
+    console.log(displayValue);
+    console.log("lastOperator:");
+    console.log(lastOperator);
+
+ }else if (value >= 0 || value <10){
+    document.getElementById("displayValue").innerHTML += parseFloat(value);
+    displayValue = document.getElementById("displayValue").innerHTML;
+    lastOperator = currentOperator;
+        console.log("----------");
+    console.log("6th");
+
+
+
   }
 }
