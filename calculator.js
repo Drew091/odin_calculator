0760e37acd3e37eab98b1757ac9c9b7c143c2229
Drew091 <hoppal.andras91@gmail.com>
2023-03-29 18:13:18 +0200

    Create "Clear", "DEL" and "." buttons and their functions

--- calculator.js
+++ calculator.js
@@ -40,10 +40,29 @@ function operate(operator,firstNumber,secondNumber) {
 }
 
 function sendToDisplay(value){
-  if (firstNumber == 0 && lastOperator == "" && (value == "+" || value == "-" || value == "/" || value == "*")){
+  if (value == "CLEAR"){
+    document.getElementById("displayValue").innerHTML = 0;
+    displayValue = "0";
+    currentOperator = "";
+    lastOperator = "";
+    firstNumber = 0;
+    secondNumber = 0;
+    console.log("----------");
+    console.log("Clear");
+    console.log("firstNumber:");
+    console.log(firstNumber);
+    console.log("secondNumber:");
+    console.log(secondNumber);
+    console.log("displayValue:");
+    console.log(displayValue);
+    console.log("currentOperator:");
+    console.log(currentOperator);
+    console.log("lastOperator:");
+    console.log(lastOperator);
+  }else if (firstNumber == 0 && lastOperator == "" && (value == "+" || value == "-" || value == "/" || value == "*")){
     firstNumber = parseFloat(displayValue);
     currentOperator = value;
-    displayValue = 0;
+    displayValue = "0";
     console.log("----------");
     console.log("1st");
     console.log("firstNumber:");
@@ -56,13 +75,13 @@ function sendToDisplay(value){
     console.log(currentOperator);
     console.log("lastOperator:");
     console.log(lastOperator);
- }else if (secondNumber == 0 && lastOperator != "" && (value == "+" || value == "-" || value == "/" || value == "*")){
+ }else if (lastOperator != "" && (value == "+" || value == "-" || value == "/" || value == "*")){
    displayValue = document.getElementById("displayValue").innerHTML;
    secondNumber = parseFloat(displayValue);
    firstNumber = parseFloat(operate(lastOperator,firstNumber,secondNumber));
    document.getElementById("displayValue").innerHTML = firstNumber ;
    secondNumber = 0;
-   displayValue = 0;
+   displayValue = "0";
    lastOperator = "";
    currentOperator = value;
    console.log("----------");
@@ -79,7 +98,7 @@ function sendToDisplay(value){
    console.log(lastOperator);
  }else if ((value == "+" || value == "-" || value == "/" || value == "*")){
     currentOperator = value;
-    displayValue = 0;
+    displayValue = "0";
     console.log("----------");
     console.log("2nd");
     console.log("firstNumber:");
@@ -92,46 +111,40 @@ function sendToDisplay(value){
     console.log(currentOperator);
     console.log("lastOperator:");
     console.log(lastOperator);
-  }else if (value == "=" && lastOperator == ""){
-    document.getElementById("displayValue").innerHTML = 0;
-
-  }else if (secondNumber == 0 && lastOperator != "" && value == "="){
-    displayValue = document.getElementById("displayValue").innerHTML;
+  }else if (value == "=" && firstNumber == 0 && secondNumber == 0){
+      document.getElementById("displayValue").innerHTML = parseFloat(displayValue);
+      console.log("----------");
+      console.log("3rd");
+      console.log("firstNumber:");
+      console.log(firstNumber);
+      console.log("secondNumber:");
+      console.log(secondNumber);
+      console.log("displayValue:");
+      console.log(displayValue);
+      console.log("currentOperator:");
+      console.log(currentOperator);
+      console.log("lastOperator:");
+      console.log(lastOperator);
+  }else if (value == "=" && displayValue == "0"){
+      document.getElementById("displayValue").innerHTML = firstNumber;
+      console.log("----------");
+      console.log("4th");
+      console.log("firstNumber:");
+      console.log(firstNumber);
+      console.log("secondNumber:");
+      console.log(secondNumber);
+      console.log("displayValue:");
+      console.log(displayValue);
+      console.log("currentOperator:");
+      console.log(currentOperator);
+      console.log("lastOperator:");
+      console.log(lastOperator);
+  }else if (currentOperator == lastOperator && value == "="){
+    lastOperator = currentOperator;
     secondNumber = parseFloat(displayValue);
     firstNumber = parseFloat(operate(lastOperator,firstNumber,secondNumber));
     document.getElementById("displayValue").innerHTML = firstNumber ;
-    displayValue = 0;
-    console.log("----------");
-    console.log("3rd");
-    console.log("firstNumber:");
-    console.log(firstNumber);
-    console.log("secondNumber:");
-    console.log(secondNumber);
-    console.log("displayValue:");
-    console.log(displayValue);
-    console.log("lastOperator:");
-    console.log(lastOperator);
-
-  }else if (value == "="){
-    displayValue = document.getElementById("displayValue").innerHTML;
-    firstNumber = parseFloat(operate(lastOperator,firstNumber,secondNumber));
-    document.getElementById("displayValue").innerHTML = firstNumber ;
-    displayValue = 0;
-    console.log("----------");
-    console.log("4th");
-    console.log("firstNumber:");
-    console.log(firstNumber);
-    console.log("secondNumber:");
-    console.log(secondNumber);
-    console.log("displayValue:");
-    console.log(displayValue);
-    console.log("lastOperator:");
-    console.log(lastOperator);
-
-  }else if (displayValue == 0){
-    document.getElementById("displayValue").innerHTML = value;
-    displayValue = document.getElementById("displayValue").innerHTML;
-    lastOperator = currentOperator;
+    lastOperator = "";
     console.log("----------");
     console.log("5th");
     console.log("firstNumber:");
@@ -140,17 +153,156 @@ function sendToDisplay(value){
     console.log(secondNumber);
     console.log("displayValue:");
     console.log(displayValue);
+    console.log("currentOperator:");
+    console.log(currentOperator);
     console.log("lastOperator:");
     console.log(lastOperator);
 
- }else if (value >= 0 || value <10){
-    document.getElementById("displayValue").innerHTML += parseFloat(value);
+  }else if (value == "="){
+    lastOperator = currentOperator;
+    displayValue = document.getElementById("displayValue").innerHTML;
+    firstNumber = parseFloat(operate(lastOperator,firstNumber,secondNumber));
+    document.getElementById("displayValue").innerHTML = firstNumber ;
+    lastOperator = "";
+    //secondNumber = 0;
+    console.log("----------");
+    console.log("6th");
+    console.log("firstNumber:");
+    console.log(firstNumber);
+    console.log("secondNumber:");
+    console.log(secondNumber);
+    console.log("displayValue:");
+    console.log(displayValue);
+    console.log("currentOperator:");
+    console.log(currentOperator);
+    console.log("lastOperator:");
+    console.log(lastOperator);
+
+  }else if (displayValue == "0" && value !="DEL" && value !="."){
+    document.getElementById("displayValue").innerHTML = value;
     displayValue = document.getElementById("displayValue").innerHTML;
     lastOperator = currentOperator;
-        console.log("----------");
-    console.log("6th");
+    console.log("----------");
+    console.log("7th");
+    console.log("firstNumber:");
+    console.log(firstNumber);
+    console.log("secondNumber:");
+    console.log(secondNumber);
+    console.log("displayValue:");
+    console.log(displayValue);
+    console.log("currentOperator:");
+    console.log(currentOperator);
+    console.log("lastOperator:");
+    console.log(lastOperator);
+  }else if (displayValue != "0." && parseFloat(displayValue) == secondNumber && (value >= 0 || value <10)){
+    document.getElementById("displayValue").innerHTML = 0;
+    displayValue = "0";
+    currentOperator = "";
+    lastOperator = "";
+    firstNumber = 0;
+    secondNumber = 0;
+    document.getElementById("displayValue").innerHTML = value;
+    displayValue = value;
+    lastOperator = currentOperator;
+    console.log("----------");
+    console.log("8th");
+    console.log("firstNumber:");
+    console.log(firstNumber);
+    console.log("secondNumber:");
+    console.log(secondNumber);
+    console.log("displayValue:");
+    console.log(displayValue);
+    console.log("currentOperator:");
+    console.log(currentOperator);
+    console.log("lastOperator:");
+    console.log(lastOperator);
+ }else if (parseFloat(displayValue) != secondNumber && (value >= 0 || value <10 )){
+    document.getElementById("displayValue").innerHTML += value;
+    displayValue = document.getElementById("displayValue").innerHTML;
+    lastOperator = currentOperator;
+    console.log("----------");
+    console.log("9th");
+    console.log("firstNumber:");
+    console.log(firstNumber);
+    console.log("secondNumber:");
+    console.log(secondNumber);
+    console.log("displayValue:");
+    console.log(displayValue);
+    console.log("currentOperator:");
+    console.log(currentOperator);
+    console.log("lastOperator:");
+    console.log(lastOperator);
+  }else if (displayValue == "0." && (value >= 0 || value <10 )){
+     document.getElementById("displayValue").innerHTML += value;
+     displayValue = document.getElementById("displayValue").innerHTML;
+     lastOperator = currentOperator;
+     console.log("----------");
+     console.log("10th");
+     console.log("firstNumber:");
+     console.log(firstNumber);
+     console.log("secondNumber:");
+     console.log(secondNumber);
+     console.log("displayValue:");
+     console.log(displayValue);
+     console.log("currentOperator:");
+     console.log(currentOperator);
+     console.log("lastOperator:");
+     console.log(lastOperator);
+  }else if ((firstNumber == 0 && secondNumber == 0 || lastOperator != "") && value == "DEL"){
 
+     displayValue = document.getElementById("displayValue").innerHTML;
+     displayValue = displayValue.slice(0, -1);
+     document.getElementById("displayValue").innerHTML = displayValue;
+     lastOperator = currentOperator;
+     if(displayValue.length ==0){
+       displayValue = "0";
+       document.getElementById("displayValue").innerHTML = 0;
+     }
+     console.log("----------");
+     console.log("11th");
+     console.log("firstNumber:");
+     console.log(firstNumber);
+     console.log("secondNumber:");
+     console.log(secondNumber);
+     console.log("displayValue:");
+     console.log(displayValue);
+     console.log("currentOperator:");
+     console.log(currentOperator);
+     console.log("lastOperator:");
+     console.log(lastOperator);
 
+   }else if (!displayValue.includes(".") && parseFloat(displayValue) != secondNumber &&  value == "."){
+      document.getElementById("displayValue").innerHTML += value;
+      displayValue = document.getElementById("displayValue").innerHTML;
+      lastOperator = currentOperator;
+      console.log("----------");
+      console.log("12th");
+      console.log("firstNumber:");
+      console.log(firstNumber);
+      console.log("secondNumber:");
+      console.log(secondNumber);
+      console.log("displayValue:");
+      console.log(displayValue);
+      console.log("currentOperator:");
+      console.log(currentOperator);
+      console.log("lastOperator:");
+      console.log(lastOperator);
+    }else if (!displayValue.includes(".") && displayValue == "0" &&  value == "."){
+       document.getElementById("displayValue").innerHTML += value;
+       displayValue = document.getElementById("displayValue").innerHTML;
+       lastOperator = currentOperator;
+       console.log("----------");
+       console.log("13th");
+       console.log("firstNumber:");
+       console.log(firstNumber);
+       console.log("secondNumber:");
+       console.log(secondNumber);
+       console.log("displayValue:");
+       console.log(displayValue);
+       console.log("currentOperator:");
+       console.log(currentOperator);
+       console.log("lastOperator:");
+       console.log(lastOperator);
 
   }
 }
